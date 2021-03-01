7cef69d335e48edde6cc52f9f775039e0069d221
levkopo <40952805+levkoposc@users.noreply.github.com>
2021-03-01 17:44:33 +0500

    Exception changes

--- src/main/java/com/github/kayjamlang/core/exceptions/ParserException.java
+++ src/main/java/com/github/kayjamlang/core/exceptions/ParserException.java
@@ -2,12 +2,12 @@ package com.github.kayjamlang.core.exceptions;
 
 import com.github.kayjamlang.core.KayJamLexer;
 
-public class CompileException extends Exception {
-    public CompileException(KayJamLexer lexer, String message){
+public class ParserException extends Exception {
+    public ParserException(KayJamLexer lexer, String message){
         super(message+" in "+lexer.getLine()+" line");
     }
 
-    public CompileException(int line, String message){
+    public ParserException(int line, String message){
         super(message+" in "+line+" line");
     }
 }
