3789f2f903756c0933a9f7f7fc858737aafaadbd
levkopo <40952805+levkoposc@users.noreply.github.com>
2021-03-02 15:37:09 +0500

    CONTEXT FOR EXECUTOR!

--- src/main/java/com/github/kayjamlang/core/provider/ExpressionProvider.java
+++ src/main/java/com/github/kayjamlang/core/provider/ExpressionProvider.java
@@ -1,10 +1,11 @@
 package com.github.kayjamlang.core.provider;
 
-public class ExpressionProvider<ExpressionType, ReturnObject> {
+public class ExpressionProvider<ExpressionType, ReturnObject, ContextObject, MainContextObject> {
 
-    public ReturnObject provide(MainExpressionProvider<ReturnObject> mainProvider,
-                                Context context,
-                                Context argsContext,
+    public ReturnObject provide(MainExpressionProvider<ReturnObject, ContextObject,
+                                        MainContextObject> mainProvider,
+                                ContextObject context,
+                                ContextObject argsContext,
                                 ExpressionType expression) throws Exception {
         return null;
     }
--- src/main/java/com/github/kayjamlang/core/provider/MainExpressionProvider.java
+++ src/main/java/com/github/kayjamlang/core/provider/MainExpressionProvider.java
@@ -7,12 +7,12 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainExpressionProvider<ReturnObject> {
+public class MainExpressionProvider<ReturnObject, ContextObject, MainContextObject> {
     private final Map<Class<? extends Expression>,
-            ExpressionProvider<? extends Expression, ReturnObject>> compilers = new HashMap<>();
+            ExpressionProvider<? extends Expression, ReturnObject, ContextObject, MainContextObject>> compilers = new HashMap<>();
     private final ReturnObject defaultObject;
 
-    public MainContext mainContext = new MainContext(null, null);
+    public MainContextObject mainContext;
 
     public MainExpressionProvider(ReturnObject defaultObject){
         this.defaultObject = defaultObject;
@@ -20,19 +20,20 @@ public class MainExpressionProvider<ReturnObject> {
 
     public <ExpressionType extends Expression> void addCompiler(
             Class<ExpressionType> expression,
-            ExpressionProvider<ExpressionType, ReturnObject> expressionCompiler) {
+            ExpressionProvider<ExpressionType, ReturnObject, ContextObject, MainContextObject> expressionCompiler) {
         compilers.put(expression, expressionCompiler);
     }
 
     @SuppressWarnings("unchecked")
-    public ReturnObject provide(Expression expression, Context context, Context argsContext) throws Exception {
+    public ReturnObject provide(Expression expression, ContextObject context,
+                                ContextObject argsContext) throws Exception {
         if(compilers.containsKey(expression.getClass())){
-            ExpressionProvider<? extends Expression, ReturnObject> expressionCompiler =
+            ExpressionProvider<? extends Expression, ReturnObject, ContextObject, MainContextObject> expressionCompiler =
                     compilers.get(expression.getClass());
             Method method = expressionCompiler.getClass()
                     .getDeclaredMethod("provide", MainExpressionProvider.class,
-                            Context.class,
-                            Context.class,
+                            Object.class,
+                            Object.class,
                             Object.class);
 
             try {
