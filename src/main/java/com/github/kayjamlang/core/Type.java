d2223ab83afea5ef6ace1dffa0be77b510c63a57
levkopo <40952805+levkopo@users.noreply.github.com>
2021-03-22 15:40:13 +0500

    DOOOOOOOOOOOOOOOOOOOOOOOUUUUUUUUUBLEEEEEEEEEEEEET

--- src/main/java/com/github/kayjamlang/core/Type.java
+++ src/main/java/com/github/kayjamlang/core/Type.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class Type {
     //Types
     public static final Type INTEGER = new Type("int", Long.class, false);
+    public static final Type DOUBLE = new Type("double", Double.class, false);
     public static final Type STRING = new Type("string", String.class, false);
     public static final Type BOOLEAN = new Type("bool", Boolean.class, false);
     public static final Type OBJECT = new Type("obj", ObjectContainer.class, false);
