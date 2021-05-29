e9023880b2e4195797622b440ee80499464ea0e7
Domaman202 <vip.domaman@mail.ru>
2021-05-29 20:08:17 +0700

    fix java:S119

--- src/main/java/com/github/kayjamlang/core/provider/ExpressionProvider.java
+++ src/main/java/com/github/kayjamlang/core/provider/ExpressionProvider.java
@@ -3,21 +3,20 @@ package com.github.kayjamlang.core.provider;
 import com.github.kayjamlang.core.Type;
 import com.github.kayjamlang.core.exceptions.TypeException;
 
-public abstract class ExpressionProvider<ExpressionType, ReturnObject, ContextObject, MainContextObject> {
-
-    public ReturnObject provide(MainExpressionProvider<ReturnObject, ContextObject,
-                                        MainContextObject> mainProvider,
-                                ContextObject context,
-                                ContextObject argsContext,
-                                ExpressionType expression) throws Exception {
+public abstract class ExpressionProvider<ET, RO, CO, MCO> {
+    /**
+     * @deprecated please replace to {@code mainProvider.defaultObject}
+     */
+    @Deprecated
+    public RO provide(MainExpressionProvider<RO, CO, MCO> mainProvider, CO context, CO argsContext, ET expression) {
         return mainProvider.defaultObject;
     }
 
-    public Type getType(MainExpressionProvider<ReturnObject, ContextObject,
-            MainContextObject> mainProvider,
-                        ContextObject context,
-                        ContextObject argsContext,
-                        ExpressionType expression) throws TypeException {
+    /**
+     * @deprecated please don't use this
+     */
+    @Deprecated
+    public Type getType(MainExpressionProvider<RO, CO, MCO> mainProvider, CO context, CO argsContext, ET expression) throws TypeException {
         return Type.ANY;
     }
 }
