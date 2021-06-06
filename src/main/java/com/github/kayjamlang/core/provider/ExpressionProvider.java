7dbc92ef309401c5c82c0dbc1a27492a0590f731
levkopo <40952805+levkopo@users.noreply.github.com>
2021-06-06 20:42:12 +0500

    Update ExpressionProvider.java

--- src/main/java/com/github/kayjamlang/core/provider/ExpressionProvider.java
+++ src/main/java/com/github/kayjamlang/core/provider/ExpressionProvider.java
@@ -4,18 +4,11 @@ import com.github.kayjamlang.core.Type;
 import com.github.kayjamlang.core.exceptions.TypeException;
 
 public abstract class ExpressionProvider<A, B, C, D> {
-    /**
-     * @deprecated please replace to {@code mainProvider.defaultObject}
-     */
-    @Deprecated
+
     public B provide(MainExpressionProvider<B, C, D> mainProvider, C context, C argsContext, A expression) {
         return mainProvider.defaultObject;
     }
 
-    /**
-     * @deprecated please don't use this
-     */
-    @Deprecated
     public Type getType(MainExpressionProvider<B, C, D> mainProvider, C context, C argsContext, A expression) throws TypeException {
         return Type.ANY;
     }
