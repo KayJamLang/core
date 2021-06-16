2dc0f3a8e7fad2cc2406a5e9402d700289df06ba
levkopo <40952805+levkopo@users.noreply.github.com>
2021-06-16 19:43:37 +0500

    variable type

--- src/main/scala/com/github/kayjamlang/core/expressions/VariableExpression.scala
+++ src/main/scala/com/github/kayjamlang/core/expressions/VariableExpression.scala
@@ -1,6 +1,10 @@
 package com.github.kayjamlang.core.expressions
 
+import com.github.kayjamlang.core.Type
 import com.github.kayjamlang.core.opcodes.AccessType
 
 class VariableExpression(val name: String,
-                         val expression: Expression, accessType: AccessType, line: Int) extends Expression(accessType, line)
+                         val expression: Expression,
+                         val variableType: Type = null,
+                         accessType: AccessType,
+                         line: Int) extends Expression(accessType, line)
