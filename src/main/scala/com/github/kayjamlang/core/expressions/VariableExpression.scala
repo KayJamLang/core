61f34e019c3e04379bcbbe2e13e475a0fc342e8e
levkopo <40952805+levkopo@users.noreply.github.com>
2021-06-16 19:27:01 +0500

    TABULATION IS 4

--- src/main/scala/com/github/kayjamlang/core/expressions/VariableExpression.scala
+++ src/main/scala/com/github/kayjamlang/core/expressions/VariableExpression.scala
@@ -2,4 +2,5 @@ package com.github.kayjamlang.core.expressions
 
 import com.github.kayjamlang.core.opcodes.AccessType
 
-class VariableExpression(val name: String, val expression: Expression, accessType: AccessType, line: Int) extends Expression(accessType, line)
+class VariableExpression(val name: String,
+                         val expression: Expression, accessType: AccessType, line: Int) extends Expression(accessType, line)
