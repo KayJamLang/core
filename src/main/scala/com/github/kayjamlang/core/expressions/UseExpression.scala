b7dd5c165dd2c02987741def9e03628250eb6c91
levkopo <40952805+levkopo@users.noreply.github.com>
2021-06-18 22:07:46 +0500

    Update UseExpression.scala

--- src/main/scala/com/github/kayjamlang/core/expressions/UseExpression.scala
+++ src/main/scala/com/github/kayjamlang/core/expressions/UseExpression.scala
@@ -5,25 +5,8 @@ import com.github.kayjamlang.core.opcodes.AccessType
 
 import scala.collection.mutable
 
-class UseExpression(line: Int, var required: ArrayList[String], val from: String, val expression: Expression) extends Expression(AccessType NONE, line) {
+class UseExpression(line: Int, var required: ArrayList[String], val from: String) extends Expression(AccessType NONE, line) {
   def this(required: ArrayList[String], from: String, line: Int) {
-    this(line, required, from, null)
-  }
-
-  /**
-   * @deprecated please don't use this
-   */
-  @deprecated
-  def this(expression: Expression, line: Int) {
-    this(line, null, null, expression)
-  }
-
-  /**
-   * @deprecated please don't use this
-   */
-  @deprecated
-  trait UseInterface {
-    @throws[Exception]
-    def getExpression(path: String): Expression
+    this(line, required, from)
   }
 }
