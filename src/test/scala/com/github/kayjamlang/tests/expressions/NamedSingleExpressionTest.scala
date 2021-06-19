b28cab2bacc33622fad45e8e404257cd0def9ec5
levkopo <40952805+levkopo@users.noreply.github.com>
2021-06-19 19:02:45 +0500

    fiiiiiiiiiiiiiiiiiiiiiix

--- src/test/scala/com/github/kayjamlang/tests/expressions/NamedSingleExpressionTest.scala
+++ src/test/scala/com/github/kayjamlang/tests/expressions/NamedSingleExpressionTest.scala
@@ -20,6 +20,7 @@ class NamedSingleExpressionTest {
     val expression = NamedSingleExpressionTest.parser.readExpression
     assertNotNull(expression)
     assertSame(classOf[NamedExpression], expression getClass)
+
     val namedContainer = expression.asInstanceOf[NamedExpression]
     assertSame(classOf[CallOrCreateExpression], namedContainer.expression getClass)
   }
