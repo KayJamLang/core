718735fd2c153a898d22c762c417b5ffd29e213b
Лев Копорушкин <levkopoag@gmail.com>
2021-08-10 21:28:30 +0500

    Revert "new stmt convertation started!"
    This reverts commit 4f23fbfb2286db6213b94e643ae22a72c3c7883f.

---
+++ src/main/scala/com/github/kayjamlang/core/containers/ClassContainer.scala
@@ -0,0 +1,39 @@
+package com.github.kayjamlang.core.containers
+
+import com.github.kayjamlang.core.ArrayList
+import com.github.kayjamlang.core.exceptions.ParserException
+import com.github.kayjamlang.core.expressions.{Expression, VariableExpression}
+import com.github.kayjamlang.core.opcodes.AccessType
+
+@deprecated("Use stmt version")
+class ClassContainer(val name: String, val extendsClass: String, val implementsClass: ArrayList[String], children: ArrayList[Expression], accessType: AccessType, line: Int) extends Container(children, accessType, line, false) {
+  var companion: ObjectContainer = null
+  var constructors = new ArrayList[ConstructorContainer]
+  var variables = new ArrayList[VariableExpression]
+
+  for (expression <- children) {
+    expression match {
+      case container: ObjectContainer if expression.accessType eq AccessType.COMPANION =>
+        if (companion != null)
+          throw new ParserException(expression.line, "companion already exists")
+        companion = container
+        children -= expression
+      case container: ConstructorContainer =>
+        constructors += container
+        children -= expression
+      case expression1: VariableExpression => variables += expression1
+      case _ => throw new ParserException(line, "The class can only contain variables and functions")
+    }
+  }
+
+  @SuppressWarnings(Array("unchecked"))
+  @throws[CloneNotSupportedException]
+  override def clone: ClassContainer = {
+    val classContainer = super.clone.asInstanceOf[ClassContainer]
+    classContainer.constructors = constructors clone()
+    classContainer.variables = variables clone()
+    classContainer
+  }
+
+  override def toString: String = s"$name@class"
+}
--- src/main/scala/com/github/kayjamlang/core/stmts/ClassStmt.scala
+++ src/main/scala/com/github/kayjamlang/core/stmts/ClassStmt.scala
@@ -6,18 +6,8 @@ class ClassStmt(
                 val name: String,
                 val classExtends: String,
                 val implementsClass: ArrayList[String],
-                var methods: ArrayList[FunctionStmt],
-                var children: ArrayList[StmtExpression],
-                var companion: ObjectStmt
+                val methods: ArrayList[FunctionStmt],
+                val children: ArrayList[StmtExpression]
                ) extends Stmt {
-  @SuppressWarnings(Array("unchecked"))
-  @throws[CloneNotSupportedException]
-  override def clone: ClassStmt = {
-    val classContainer = super.clone.asInstanceOf[ClassStmt]
-    classContainer.methods = methods clone()
-    classContainer.children = children clone()
-    classContainer
-  }
 
-  override def toString: String = s"$name@class"
 }
