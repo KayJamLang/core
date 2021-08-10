df6bd0b1c6eedca4d863bc03f6b7b55b35662f3d
Лев Копорушкин <levkopoag@gmail.com>
2021-08-10 21:34:13 +0500

    Revert "."
    This reverts commit cdd56b9ead777cddb80bcff951d3f27d795578a9.

--- src/main/scala/com/github/kayjamlang/core/expressions/VariableSetExpression.scala
+++ src/main/scala/com/github/kayjamlang/core/expressions/VariableSetExpression.scala
@@ -2,4 +2,4 @@ package com.github.kayjamlang.core.expressions
 
 import com.github.kayjamlang.core.opcodes.AccessType
 
-class VariableSetExpression(name: String, expression: Expression, line: Int) extends VariableExpression(name, expression, null, AccessType.NONE, line)
+class VariableSetExpression(name: String, expression: Expression, line: Int) extends VariableExpression(name, expression, AccessType.NONE, line)
