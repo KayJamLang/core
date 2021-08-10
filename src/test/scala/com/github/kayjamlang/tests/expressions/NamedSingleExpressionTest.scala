65ca2c555fc97311fdd1b07944970de2eb1a01a9
Лев Копорушкин <levkopoag@gmail.com>
2021-08-10 21:29:17 +0500

    Revert "Update NamedSingleExpressionTest.scala"
    This reverts commit 3a270d38b81ded34dbeb4b857da3a6468cdafb53.

--- src/test/scala/com/github/kayjamlang/tests/expressions/NamedSingleExpressionTest.scala
+++ src/test/scala/com/github/kayjamlang/tests/expressions/NamedSingleExpressionTest.scala
@@ -1,56 +1,27 @@
 package com.github.kayjamlang.tests.expressions
 
 import com.github.kayjamlang.core.{KayJamLexer, KayJamParser}
-import com.github.kayjamlang.core.expressions.{CallOrCreateExpression, FunctionRefExpression, NamedExpression}
+import com.github.kayjamlang.core.expressions.{CallOrCreateExpression, NamedExpression}
 import org.junit.Assert.{assertNotNull, assertSame}
 import org.junit.{BeforeClass, Test}
+
+object NamedSingleExpressionTest {
+  private var parser: KayJamParser = null
+
+  @BeforeClass def prepare(): Unit = {
+    parser = new KayJamParser(new KayJamLexer("test -> println()"))
+  }
+}
 
 class NamedSingleExpressionTest {
   @Test
   @throws[Exception]
-  def test1(): Unit = { // TODO: rename test!
-    val parser = new KayJamParser(new KayJamLexer("test -> () { println() }"))
-
-    val expression = parser.readExpression
+  def test(): Unit = {
+    val expression = NamedSingleExpressionTest.parser.readExpression
     assertNotNull(expression)
     assertSame(classOf[NamedExpression], expression getClass)
 
     val namedContainer = expression.asInstanceOf[NamedExpression]
-    assertSame(classOf[FunctionRefExpression], namedContainer.expression getClass)
-
-    val functionRefContainer = namedContainer.expression.asInstanceOf[FunctionRefExpression]
-    assertSame(classOf[CallOrCreateExpression], functionRefContainer.expression getClass)
-  }
-
-  @Test
-  @throws[Exception]
-  def test2(): Unit = { // TODO: rename test!
-    val parser = new KayJamParser(new KayJamLexer("test -> { println() }"))
-
-    val expression = parser.readExpression
-    assertNotNull(expression)
-    assertSame(classOf[NamedExpression], expression getClass)
-
-    val namedContainer = expression.asInstanceOf[NamedExpression]
-    assertSame(classOf[FunctionRefExpression], namedContainer.expression getClass)
-
-    val functionRefContainer = namedContainer.expression.asInstanceOf[FunctionRefExpression]
-    assertSame(classOf[CallOrCreateExpression], functionRefContainer.expression getClass)
-  }
-
-  @Test
-  @throws[Exception]
-  def test3(): Unit = { // TODO: rename test!
-    val parser = new KayJamParser(new KayJamLexer("test -> println()"))
-
-    val expression = parser.readExpression
-    assertNotNull(expression)
-    assertSame(classOf[NamedExpression], expression getClass)
-
-    val namedContainer = expression.asInstanceOf[NamedExpression]
-    assertSame(classOf[FunctionRefExpression], namedContainer.expression getClass)
-
-    val functionRefContainer = namedContainer.expression.asInstanceOf[FunctionRefExpression]
-    assertSame(classOf[CallOrCreateExpression], functionRefContainer.expression getClass)
+    assertSame(classOf[CallOrCreateExpression], namedContainer.expression getClass)
   }
 }
