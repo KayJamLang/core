3ba4ede496d82404ffca922a29f98cc38ebf5e6d
Лев Копорушкин <levkopoag@gmail.com>
2021-08-10 21:39:56 +0500

    Revert "TABULATION IS 4"
    This reverts commit 61f34e019c3e04379bcbbe2e13e475a0fc342e8e.

--- src/main/scala/com/github/kayjamlang/core/expressions/VariableExpression.scala
+++ src/main/scala/com/github/kayjamlang/core/expressions/VariableExpression.scala
@@ -2,5 +2,4 @@ package com.github.kayjamlang.core.expressions
 
 import com.github.kayjamlang.core.opcodes.AccessType
 
-class VariableExpression(val name: String,
-                         val expression: Expression, accessType: AccessType, line: Int) extends Expression(accessType, line)
+class VariableExpression(val name: String, val expression: Expression, accessType: AccessType, line: Int) extends Expression(accessType, line)
