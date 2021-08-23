dbfb5b5bbcbd71e9ca1e4c25cf6945ae10d14a1b
Лев Копорушкин <levkopoag@gmail.com>
2021-08-23 12:26:12 +0500

    Update KayJamLexerException.java

--- src/main/java/com/github/kayjamlang/core/exceptions/KayJamLexerException.java
+++ src/main/java/com/github/kayjamlang/core/exceptions/KayJamLexerException.java
@@ -4,6 +4,6 @@ import com.github.kayjamlang.core.KayJamLexer;
 
 public class KayJamLexerException extends Exception {
     public KayJamLexerException(KayJamLexer lexer, String errorMessage) {
-        super(errorMessage + " in " + lexer.source.path);
+        super("Lexer error: "+ errorMessage + " on "+lexer.getLine()+" in " + lexer.source.path);
     }
 }
